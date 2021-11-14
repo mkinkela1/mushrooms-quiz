4644e3f9ea90f4b9daf15342f64a9379f688c84c
Matteo Kinkela <matteo.kinkela@reev.hr>
2021-11-14 19:50:03 +0100

    mushrooms app

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+interface IMushroom {
+  nameCro: string,
+  nameLat: string,
+  img: string,
+  sortVal: number
+}
+
+const App = () => {
+
+  const [mushrooms, setMushrooms] = useState<IMushroom[]>([]);
+  const [idx, setIdx] = useState<number> (0);
+  const [revealAnswer, setRevealAnswer] = useState<boolean> (false);
+
+  useEffect(() => {
+    (async () => {
+      const data = await (await fetch("/data/mushrooms.json")).json();
+
+      const arr: IMushroom[] = data.map((mushroom: IMushroom) => ({ ...mushroom, sortVal: Math.random() })).sort((a: IMushroom, b: IMushroom) => a.sortVal - b.sortVal);
+      setMushrooms(arr);
+    })();
+  }, []);
+
+  const showAnswer = () => setRevealAnswer(true);
+  const next = () => {
+
+    if(idx + 1 >= mushrooms.length)
+      setIdx(0);
+    else
+      setIdx(prevState => prevState + 1);
+
+    setRevealAnswer(false);
+  };
+
+  if(mushrooms.length > 0)
+    return (
+        <div className="App">
+          <img src={`assets/mushrooms/${mushrooms[idx].img}`} alt={mushrooms[idx].nameCro} />
+          {
+            revealAnswer &&
+            <div className="veggie-data">
+              <table>
+                <tr><td>Naziv latinski: </td><td>{mushrooms[idx].nameLat}</td></tr>
+                <tr><td>Naziv hrvatski: </td><td>{mushrooms[idx].nameCro}</td></tr>
+              </table>
+            </div>
+          }
+          {
+            !revealAnswer ?
+                <button className="btn" onClick={() => showAnswer()}>Prikaži odgovor</button>
+                :
+                <button className="btn" onClick={() => next()}>Dalje</button>
+          }
+        </div>
+    );
+  else
+    return (<>Loading</>)
+}
+
+export default App;
